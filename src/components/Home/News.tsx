2dba7e2b21b81276ea212280e6fd25e408e4381c
陳翰毅 <tp6c04u4456@yahoo.com.tw>
2025-01-06 23:16:03 +0800

    修改部分圖片路徑，gh-page上無法顯示的部分

--- src/components/Home/News.tsx
+++ src/components/Home/News.tsx
@@ -6,7 +6,7 @@ export default function News() {
     <>
       <div className='relative grid md:grid-cols-12 gap-4 p-14 w-full bg-[#F7F2EE]'>
         <img
-          src="/images/web/dot.png"
+          src={`${import.meta.env.BASE_URL}images/web/dot.png`}
           alt="Top Right Dots"
           className="absolute top-10 right-28 h-36"
         />
@@ -24,7 +24,7 @@ export default function News() {
           <NewsCard />
         </div>
         <img
-          src="/images/web/dot.png"
+          src={`${import.meta.env.BASE_URL}images/web/dot.png`}
           alt="Dots Bottom Left"
           className="absolute bottom-[-4.5rem] left-32 h-36"
         />
