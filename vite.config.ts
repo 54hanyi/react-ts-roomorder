7af354edf46c7cddab01db8c4a0e2c0ef1cf4997
陳翰毅 <tp6c04u4456@yahoo.com.tw>
2025-01-06 19:58:29 +0800

    測試圖片路徑

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+export default defineConfig(() => {
+  // const env = loadEnv(mode, process.cwd(), '');
   return {
-    base: env.VITE_BASE_URL || '/react-ts-roomorder/',
+    base:'/react-ts-roomorder/',
     plugins: [
       react(),
       createSvgIconsPlugin({
